f106fa9c05b1bc3f2ea4a9ded681947f27c50608
Chris McComb <ccmcc2012@gmail.com>
2015-02-18 12:25:15 -0500

    Update example.m

--- example.m
+++ example.m
@@ -1,3 +1,5 @@
+% Based on http://www.mathworks.com/matlabcentral/fileexchange/35292-matlab-plot-gallery-graph-plot/content/html/Graph_Plot.html
+
 % Get connectivity and node locations for a buckyball
 [B, V] = bucky;
 
