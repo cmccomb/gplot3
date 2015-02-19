c83ff63e2e59fab45dda8127ae38bd0d25301583
Chris McComb <ccmcc2012@gmail.com>
2015-02-19 14:27:30 -0500

    Logic and robustness
    More clear organization, handles more cases for inputs gracefully.

--- gplot3.m
+++ gplot3.m
@@ -1,47 +1,50 @@
-function [Xout, Yout, Zout]=gplot3(A, xyz, linespec)
+function [X, Y, Z] = gplot3(A, xyz, linespec)
 %GPLOT Plot graph, as in "graph theory".
 %   GPLOT(A, xyz) plots the graph specified by the adjacency matrix,
-%   A, and the n-by-3 coordinates array, xyz.
+%   A, and the n-by-3 coordinate array, xyz.
 %   
 %   GPLOT(A, xyz, linespec) uses line type and color specified in the
 %   string LineSpec. See PLOT for possibilities.
 %
-%   [X,Y,Z] = GPLOT(A, xyz) returns the vectors X, Y, and Z without 
+%   [X, Y, Z] = GPLOT(A, xyz) returns the vectors X, Y, and Z without 
 %   actually generating a plot.
 
-[i,j] = find(A);
-[~, p] = sort(max(i,j));
-i = i(p);
-j = j(p);
+% If no arguments given, then run buckminster sphere example
+if nargin == 0
+    [A, xyz] = bucky;
+end
 
+% If only one argument given, throw error.
+if nargin == 1
+    error('Please provide an adjacency matrix and coordinate array');
+end
+
+% Returns i and j, lists of connected nodes
+[i,j] = find(A);
+
+% Extact 
 X = [ xyz(i,1) xyz(j,1)]';
 Y = [ xyz(i,2) xyz(j,2)]';
 Z = [ xyz(i,3) xyz(j,3)]';
 
-if isfloat(xyz) || nargout ~= 0
-    X = [X; NaN(size(i))'];
-    Y = [Y; NaN(size(i))'];
-    Z = [Z; NaN(size(i))'];
+% Add NaN values to break between line segments
+X = [X; NaN(size(i))'];
+Y = [Y; NaN(size(i))'];
+Z = [Z; NaN(size(i))'];
+
+% Serialize the x and y data
+X = X(:);
+Y = Y(:);
+Z = Z(:);
+
+% If no linespec given, then plot with default
+if nargout == 0 && nargin == 2
+    plot3(X, Y, Z);
 end
 
-if nargout == 0
-    if ~isfloat(xyz)
-        if nargin < 3
-            lc = '';
-        end
-        [lsty, csty, msty] = gplotGetRightLineStyle(gca, linestyle);    
-        plot3(X, Y, Z, linestyle);
-    else
-        if nargin < 3
-            plot3(X(:), Y(:), Z(:));
-        else
-            plot3(X(:), Y(:), Z(:), linestyle);
-        end
-    end
-else
-    Xout = X(:);
-    Yout = Y(:);
-    Zout = Z(:);
+% If linespec given, use it for plotting
+if nargout == 0 && nargin == 3 
+    plot3(X, Y, Z, linespec);
 end
 
 end
