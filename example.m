e263966dbb92d4c2bd23c5bcdb699766022563c2
Chris McComb <ccmcc2012@gmail.com>
2015-02-19 14:37:32 -0500

    Delete example.m

--- example.m
+++
@@ -1,7 +0,0 @@
-% Based on http://www.mathworks.com/matlabcentral/fileexchange/35292-matlab-plot-gallery-graph-plot/content/html/Graph_Plot.html
-
-% Get connectivity and node locations for a buckyball
-[B, V] = bucky;
-
-% Plot the 3d graph
-gplot3(B, V);
